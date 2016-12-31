d86cca9aa60061ee8f7925e21f6420081a3a7102
Lee Dohm <lee@lee-dohm.com>
2016-12-30 20:29:55 -0800

    Prevent symlinked packages from showing up in the list

--- spec/dalek-spec.js
+++ spec/dalek-spec.js
@@ -1,20 +1,35 @@
 /** @babel */
 
+const fs = require('fs')
+
 const dalek = require('../lib/dalek')
 
 describe('dalek', function () {
   describe('enumerate', function () {
+    let realPaths = {}
+
     beforeEach(function () {
       atom.devMode = false
+
+      spyOn(fs, 'realpathSync').andCallFake((filepath) => {
+        if (realPaths[filepath]) {
+          return realPaths[filepath]
+        }
+
+        return filepath
+      })
+
       spyOn(atom.packages, 'getAvailablePackagePaths').andReturn([
         '/Users/username/.atom/packages/advanced-open-file',
         '/Users/username/.atom/packages/bookmarks',
-        '/Applications/Atom.app/Contents/Resources/app.asar/node_modules/bookmarks'
+        '/Users/username/.atom/packages/foo',
+        '/Applications/Atom.app/Contents/Resources/app.asar/node_modules/bookmarks',
+        '/Applications/Atom.app/Contents/Resources/app.asar/node_modules/foo'
       ])
     })
 
     it('returns a list of duplicate names', function () {
-      expect(dalek.enumerate()).toEqual(['bookmarks'])
+      expect(dalek.enumerate()).toEqual(['bookmarks', 'foo'])
     })
 
     describe('when in dev mode', function () {
@@ -24,6 +39,16 @@ describe('dalek', function () {
 
       it('always returns an empty list', function () {
         expect(dalek.enumerate()).toEqual([])
+      })
+    })
+
+    describe('when a package is symlinked into the package directory', function () {
+      beforeEach(function () {
+        realPaths['/Users/username/.atom/packages/bookmarks'] = '/Users/username/bookmarks'
+      })
+
+      it('is not included in the list of duplicate names', function () {
+        expect(dalek.enumerate()).toEqual(['foo'])
       })
     })
   })
